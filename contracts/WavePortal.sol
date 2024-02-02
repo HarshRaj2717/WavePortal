17839f352e570b2b9f584393403749fa30350a5a
harshraj2717 <harshraj2717@gmail.com>
2024-02-03 01:59:48 +0530

    add a starting contract

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
+pragma solidity ^0.8.19;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("heyyyaaaa!!!");
+    }
+}
