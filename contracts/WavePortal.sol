7df86be34a4ef76eccc3b3b8d8be0c349db62734
harshraj2717 <harshraj2717@gmail.com>
2024-02-03 03:22:22 +0530

    make random waves

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.19;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 waves = 0;
+
     constructor() {
         console.log("heyyyaaaa!!!");
+    }
+
+    function wave() public {
+        waves += 1;
+        console.log("%s sent a wave!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We got total %d waves", waves);
+        return waves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,7 +1,19 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
+
   console.log("Okie,, deployed at : ", await waveContract.getAddress());
+  console.log("Deployed by : ", await owner.getAddress());
+
+  await waveContract.getTotalWaves();
+  const waveTxn = await waveContract.wave();
+  waveTxn.wait();
+
+  const waveTxn_2 = await waveContract.connect(randomPerson).wave();
+  waveTxn_2.wait();
+
+  await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
