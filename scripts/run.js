bb800f33027e3bc4467efa35b83b089ddef8000d
harshraj2717 <harshraj2717@gmail.com>
2024-02-03 02:16:30 +0530

    add a base script

---
+++ scripts/run.js
@@ -0,0 +1,17 @@
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  console.log("Okie,, deployed at : ", await waveContract.getAddress());
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
